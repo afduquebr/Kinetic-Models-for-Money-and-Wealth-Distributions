7f46c0fa88f9944b00a5dec1ac1d44dca521f551
avillacobh <avillacobh@unal.edu.co>
2020-07-23 10:26:09 -0500

    Probando algunas cosas

--- Wealth Distributions/Python Code/main.py
+++ Wealth Distributions/Python Code/main.py
@@ -4,7 +4,7 @@ import plot as pl
 
 
 def main():
-    N = 10000  # N es el número de agentes
+    N = 1000  # N es el número de agentes
     alpha = 1  # alpha es el dinero promedio
     beta = 1  # beta es la cantidad de bienes promedio
     T = 20 * N  # Tiempo de evolución
@@ -12,14 +12,13 @@ def main():
     a = np.full(N, alpha)
     b = np.full(N, beta)
     S = np.zeros(T)  # Entropía para cada tiempo
-    M = 1
+    M = 10
     bins = np.arange(0, 4, 0.1)
-    #pl.histogram(N, alpha, beta, T, w, a, b, bins, S, M)
-    #pl.plot(bins, pl.gamma(bins, w), 4, 1, "Distribución de riqueza (w=1)", "Riqueza",
-            #"Densidad de Probabilidad", "lightsalmon", "Gamma(1,1)")
-    s.sim(N, alpha, beta, T, w, a, b, bins, S)
-    pl.plot(range(T), S, T, 1.3, "Entropía de Shannon", "Iteración",
-            "Entropía [bits]", "limegreen", "Shannon's Entropy")
+    pl.histogram(N, alpha, beta, T, w, a, b, bins, S, M)
+    pl.plot(bins, pl.gamma(bins, w), 4, 1, "Distribución de riqueza (w=1)", "Riqueza",
+            "Densidad de Probabilidad", "lightsalmon", "Gamma(1,1)")
+    #s.sim(N, alpha, beta, T, w, a, b, bins, S)
+    #pl.plot(range(T), S, T, 1.3, "Entropía de Shannon", "Iteración","Entropía [bits]", "limegreen", "Shannon's Entropy")
 
 
 main()
