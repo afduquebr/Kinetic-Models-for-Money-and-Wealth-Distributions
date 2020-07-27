99bb2927a8f8282167c075658d30e4762d1312fa
afduquebr <afduquebr@unal.edu.co>
2020-07-26 21:53:59 -0500

    Update system.py

--- Wealth Distributions/Python Code/system.py
+++ Wealth Distributions/Python Code/system.py
@@ -3,14 +3,14 @@ import numpy as np
 
 def sim(N, alpha, beta, T, w, a, b, bins, S, mu, sigma):
     for i in range(1, T+1):
-        #f = np.random.uniform(0, 1, N)
-        f = blockednormal(mu,sigma,N)
+        f = np.random.uniform(0, 1, N)
+        #f = blockednormal(mu,sigma,N)
         p = price(a, b, f)
         m = wealth(a, b, p)
         a = f * m
         b = (1 - f) * m / p
-        #mw = w * m /(alpha + (beta * p))
-        #S[i-1] = entropy(mw, bins)
+        mw = w * m /(alpha + (beta * p))
+        S[i-1] = entropy(mw, bins)
     mw = w * m /(alpha + (beta * p))
     return mw
 
@@ -28,7 +28,7 @@ def wealth(a, b, p):
 
 def entropy(v, bins):
     m, bins = np.histogram(v, bins=bins, density=True)
-    return np.sum(np.where(m != 0,  - m * np.log2(m) * bins[1], 0))
+    return np.sum(np.where(m != 0, - m * np.log2(m) * bins[1], 0))
 
 def blockednormal(mu,sigma,N):
     random = np.zeros(N)
